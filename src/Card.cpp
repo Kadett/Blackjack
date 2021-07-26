2e69f1e951bc6f15d6ed22b9ff87cc50916ad89e
Evgen <Kadett0303@gmail.com>
2021-07-26 03:55:26 +0300

    completed class Card();
    update src/Card.h
    update src/Catd.cpp

--- src/Card.cpp
+++ src/Card.cpp
@@ -1,5 +1,11 @@
-//
-// Created by Evgeniy Onishchenko on 026 26.07.21.
-//
-
 #include "Card.h"
+
+Card::Card(Card::suits suit, Card::rank value) : m_Suit(suit), m_Rank(value) {}
+
+void Card::Flip() {
+    m_IsFaceUp = !m_IsFaceUp;
+}
+
+int Card::GetValue() {
+    return (int)m_Rank;
+}
--- src/Card.h
+++ src/Card.h
@@ -1,13 +1,17 @@
-//
-// Created by Evgeniy Onishchenko on 026 26.07.21.
-//
-
 #pragma once
-
 class Card {
-
+public:
+    enum class suits {
+        HEART, DIAMOND, CLUB, SPADE
+    };
+    enum class rank{
+        ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    };
+    Card(suits, rank);
+    void Flip();
+    int GetValue();
+private:
+    bool m_IsFaceUp{};
+    suits m_Suit;
+    rank m_Rank;
 };
-
-
-
-
